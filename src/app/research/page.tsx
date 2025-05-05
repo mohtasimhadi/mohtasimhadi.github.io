f39d9e8e8213bd3dcf9f958a34ea9279d030f836
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2025-05-05 18:04:44 -0500

    research almost done

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
 import NewsCard from "@/components/NewsCard";
-import { useEffect, useState } from 'react';
-import Card from '@/components/Card';
-import { FC } from 'react';
-import Link from 'next/link';
+import { useEffect, useState } from "react";
+import Card from "@/components/Card";
+import { FC } from "react";
+import Link from "next/link";
 
 interface Projects {
   title: string;
@@ -21,7 +21,7 @@ const Research: FC = () => {
   const [visibleNews, setVisibleNews] = useState(6);
   const [currentProjects, setCurrentProjects] = useState<Projects[]>([]);
   const [visibleProjects, setVisibleProjects] = useState<Projects[]>([]);
-  const [maxCards, setMaxCards] = useState(3);
+  const [maxCards, setMaxCards] = useState(4);
 
   useEffect(() => {
     fetch("/data/news.json")
@@ -29,21 +29,25 @@ const Research: FC = () => {
       .then((data) => setNews(data.news))
       .catch((err) => console.error("Error fetching news:", err));
 
-    fetch('/data/blogs.json')
+    fetch("/data/blogs.json")
       .then((res) => res.json())
       .then((data) => {
-        const currentProjectItems = data.filter((item: Projects) => item.type === 'current project');
+        const currentProjectItems = data.filter(
+          (item: Projects) => item.type === "current project"
+        );
         setCurrentProjects(currentProjectItems);
         setVisibleProjects(currentProjectItems.slice(0, maxCards));
       })
-      .catch((err) => console.error('Error fetching blog data:', err));
+      .catch((err) => console.error("Error fetching blog data:", err));
   }, [maxCards]);
 
   return (
     <div className="flex flex-wrap">
       {/* Left Column */}
-      <div className="w-full sm:w-1/5 p-4 border-l flex flex-col items-start">
-        <h3 className='text-xl font-semibold mb-4 text-left'>Current Projects</h3>
+      <div className="w-full sm:w-1/5 p-4 flex flex-col items-start">
+        <h3 className="text-xl font-semibold mb-4 text-left">
+          Current Projects
+        </h3>
         <div className="flex flex-col items-center w-full">
           {visibleProjects.map((project, index) => (
             <Card
@@ -58,19 +62,21 @@ const Research: FC = () => {
             />
           ))}
         </div>
-        {/* "See More" button redirects to /projects */}
-        <Link href="/projects">
-          <button className="mt-4 text-blue-600 hover:underline">
-            See More
-          </button>
-        </Link>
+
+        <div className="flex justify-end w-full mt-4">
+          <Link href="/patents">
+            <button className="text-blue-600 hover:underline">
+              See More
+            </button>
+          </Link>
+        </div>
       </div>
 
       {/* Center Column */}
-      <div className="w-full sm:w-3/5 p-4 border-l flex flex-col items-start">
+      <div className="w-full sm:w-3/5 flex flex-col items-start">
         {/* Patents Section */}
-        <div className="p-4 bg-gray-100">
-          <h3 className='text-xl font-semibold mb-4 text-left'>Patents</h3>
+        <div className="p-4 bg-amber-50">
+          <h3 className="text-xl font-semibold mb-4 text-left">Patents</h3>
           <div className="flex flex-wrap justify-center">
             {currentProjects.slice(0, 3).map((project, index) => (
               <div key={index} className="w-full sm:w-1/2 md:w-1/3 p-2">
@@ -86,18 +92,21 @@ const Research: FC = () => {
               </div>
             ))}
           </div>
-          <Link href="/projects">
-            <button className="mt-4 text-blue-600 hover:underline">
-              See More
-            </button>
-          </Link>
+          <div className="flex justify-end w-full mt-4">
+            <Link href="/projects">
+              <button className="text-blue-600 hover:underline">
+                See More
+              </button>
+            </Link>
+          </div>
         </div>
 
-        {/* Publications Section */}
-        <div className="mt-8 p-4 bg-gray-200">
-          <h3 className='text-xl font-semibold mb-4 text-left'>Publications</h3>
+        <div className="p-4 border-b-1 border-gray-400">
+          <h3 className="text-xl font-semibold mb-4 text-left">
+            Peer Reviewed Articles
+          </h3>
           <div className="flex flex-wrap justify-center">
-            {currentProjects.slice(3, 6).map((project, index) => (
+            {currentProjects.slice(0, 3).map((project, index) => (
               <div key={index} className="w-full sm:w-1/2 md:w-1/3 p-2">
                 <Card
                   title={project.title}
@@ -111,18 +120,21 @@ const Research: FC = () => {
               </div>
             ))}
           </div>
-          <Link href="/projects">
-            <button className="mt-4 text-blue-600 hover:underline">
-              See More
-            </button>
-          </Link>
+          <div className="flex justify-end w-full mt-4">
+            <Link href="/projects">
+              <button className="text-blue-600 hover:underline">
+                See More
+              </button>
+            </Link>
+          </div>
         </div>
 
-        {/* Presentations Section */}
-        <div className="mt-8 p-4 bg-gray-300">
-          <h3 className='text-xl font-semibold mb-4 text-left'>Presentations</h3>
+        <div className="p-4">
+          <h3 className="text-xl font-semibold mb-4 text-left">
+            Presentations
+          </h3>
           <div className="flex flex-wrap justify-center">
-            {currentProjects.slice(6, 9).map((project, index) => (
+            {currentProjects.slice(0, 3).map((project, index) => (
               <div key={index} className="w-full sm:w-1/2 md:w-1/3 p-2">
                 <Card
                   title={project.title}
@@ -136,31 +148,37 @@ const Research: FC = () => {
               </div>
             ))}
           </div>
-          <Link href="/projects">
-            <button className="mt-4 text-blue-600 hover:underline">
-              See More
-            </button>
-          </Link>
+          <div className="flex justify-end w-full mt-4">
+            <Link href="/projects">
+              <button className="text-blue-600 hover:underline">
+                See More
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
 
       {/* Right Column */}
-      <div className="w-full sm:w-1/5 p-4 border-l flex flex-col items-start">
-        <h3 className='text-xl font-semibold mb-4 text-left'>Latest News</h3>
+      <div className="w-full sm:w-1/5 p-4 flex flex-col items-start">
+        <h3 className="text-xl font-semibold mb-4 text-left">Latest News</h3>
         <div className="grid grid-cols-1 gap-2">
           {news.length > 0 ? (
-            news.slice(0, visibleNews).map((item, index) => (
-              <NewsCard key={index} date={item.date} news={item.news} />
-            ))
+            news
+              .slice(0, visibleNews)
+              .map((item, index) => (
+                <NewsCard key={index} date={item.date} news={item.news} />
+              ))
           ) : (
             <p>No news found.</p>
           )}
         </div>
-        <Link href="/projects">
-          <button className="mt-4 text-blue-600 hover:underline">
-            See More
-          </button>
-        </Link>
+        <div className="flex justify-end w-full mt-4">
+          <Link href="/projects">
+            <button className="text-blue-600 hover:underline">
+              See More
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
