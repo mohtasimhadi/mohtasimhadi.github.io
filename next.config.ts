d6f024d93837c0a9dd80703592cd3ef1585ab02c
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2025-05-07 23:41:13 -0500

    deloyable changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/mohtasimhadi.github.io' : '',
+  basePath: isProd ? '/mohtasimhadi.github.io' : '',
+  output: 'export'
 };
 
 export default nextConfig;
