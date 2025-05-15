c83402423d3a249a7101f4b6b98b883102e8a260
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2025-05-15 12:14:38 -0500

    section

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,5 +1,7 @@
+import Section from "@/components/Section";
+
 export default function News() {
   return (
-    <div>Hello World!</div>
+    <Section type="News"/>
   );
 }
---
+++ src/components/Section.tsx
@@ -0,0 +1,71 @@
+'use client'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Card from '@/components/Card'
+
+export default function Section({ type }: { type: string }) {
+    const [pages, setPages] = useState<any[]>([])
+    const [cursor, setCursor] = useState<string | null>(null)
+    const [hasMore, setHasMore] = useState(true)
+    const [loading, setLoading] = useState(false)
+
+    const fetchPages = async (cursor: string | null = null) => {
+        setLoading(true)
+        try {
+            const res = await axios.post('/api/notion', {
+                cursor,
+                filters: {
+                    and: [
+                        {
+                            property: "Type",
+                            select: {
+                                equals: type
+                            }
+                        }
+                    ]
+                }
+            })
+
+
+            setPages((prev) => [...prev, ...res.data.results])
+            setCursor(res.data.next_cursor)
+            setHasMore(res.data.has_more)
+        } catch (err) {
+            console.error(err)
+        }
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchPages()
+    }, [])
+
+    return (
+        <main className="max-w-6xl mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {pages.map((page) => (
+                    <Card
+                        key={page.id}
+                        variant="normal"
+                        title={page.title}
+                        cover={page.cover}
+                        date={page.date}
+                        authors={page.authors}
+                        keywords={page.tags}
+                    />
+                ))}
+            </div>
+            {hasMore && (
+                <div className="mt-6 text-center">
+                    <button
+                        onClick={() => fetchPages(cursor)}
+                        className="py-2 bg-black text-white rounded-lg hover:bg-gray-800 transition disabled:opacity-50"
+                        disabled={loading}
+                    >
+                        {loading ? 'Loading...' : 'Load More'}
+                    </button>
+                </div>
+            )}
+        </main>
+    )
+}
