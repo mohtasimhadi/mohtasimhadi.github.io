4d61c71199ad31e04147a9a440a5be2d9654b70f
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2025-05-21 02:42:09 -0500

    homepage

--- src/components/About/Description.tsx
+++ src/components/About/Description.tsx
@@ -82,13 +82,13 @@ export default function AboutSection() {
                             <div className="flex gap-4 mt-2">
                                 <button
                                     onClick={() => handleNavigation('/blogs')}
-                                    className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-400 transition"
+                                    className="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-500 transition"
                                 >
                                     Blogs →
                                 </button>
                                 <button
                                     onClick={() => handleNavigation('/research')}
-                                    className="px-4 py-2 border border-gray-600 text-gray-900 rounded hover:bg-gray-200 transition"
+                                    className="px-4 py-2 border border-gray-800 text-gray-900 rounded hover:bg-gray-200 transition"
                                 >
                                     My Research →
                                 </button>
